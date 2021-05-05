87ce99b0bcd0efb5c33165ff8aab699d0ca73a38
Luc-Wshr <71008362+Luc-Wshr@users.noreply.github.com>
2021-05-05 10:10:41 +0200

    Changed code-syntax

--- Main.py
+++ Main.py
@@ -2,19 +2,19 @@ import requests, json
 
 api_key = "3a5ee960a96dd608673bce4a6288a65c"
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
-city_name = input("Enter city name : ")
+city_name = input("From where do you want to know the weather? : ")
 complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
 response = requests.get(complete_url)
   
-x = response.json()
+get_responses = response.json()
 
-if x["cod"] != "404":
-    y = x["main"]
+if get_responses["cod"] != "404":
+    y = get_responses["main"]
     current_temperature = y["temp"]
     current_pressure = y["pressure"]
     current_humidiy = y["humidity"]
-    z = x["weather"]
+    z = get_responses["weather"]
     weather_description = z[0]["description"]
 
     print(" Temperatur (in kelvin) = " +
