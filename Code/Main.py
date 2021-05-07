ebf8edf5f9c9dec3208bda53db597189aaea8e7c
Luc Weishar <71008362+Luc-Wshr@users.noreply.github.com>
2021-05-07 11:11:04 +0200

    Update Main.py

--- Code/Main.py
+++ Code/Main.py
@@ -1,12 +1,35 @@
+# Library imports
+
 import requests, json
+import datetime
 
-api_key = "12f04c87d16f8e311477842c595d4c77"
+# Dates
 
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-city_name = input("Enter city name : ")
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-response = requests.get(complete_url)
-x = response.json()
+date_time = datetime.datetime.now()
+day = Label(root, text = date_time.strftime('%A--'), bg='white', font=("bold", 15))
+month_time = Label(root, text = date_time.strftime('%m %B'), bg='white', font=("bold", 15))
+
+# Daytimes (hours)
+
+time_hour = Label(root, text=dt.strftime('%I : %M %p'), bg='white', font=("bold", 15))
+
+# Location research
+
+name_city = StringVar()
+input_city = Entry(root, textvariable = name_city, width=45)
+
+def name_city():
+    
+    # Request API
+    
+    api_key = "12f04c87d16f8e311477842c595d4c77"
+    base_url = "https://api.openweathermap.org/data/2.5/weather?q="
+    request_weather_data = requests.get(base_url + input_city.get() + "&units=metric&appid=" + api_key)
+    x = json.loads(request_weather_data.content)
+
+    # Temp
+    
+
 
 if x["cod"] != "404":
     y = x["main"]
