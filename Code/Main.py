c67297f72f400eafa92702ae7fffcba62b068d8a
Luc-Wshr <luc.weishar@cte.ch>
2021-05-25 10:09:20 +0200

    updated main.py

--- Code/Main.py
+++ Code/Main.py
@@ -14,12 +14,12 @@ root['background'] = "white"
 # Dates
 
 date_time = datetime.datetime.now()
-day = Label(root, text = date_time.strftime('%A--'), bg='white', font=("bold", 15))
-month_time = Label(root, text = date_time.strftime('%m %B'), bg='white', font=("bold", 15))
+day = Label(root, text = date_time.strftime('%A--'))
+month_time = Label(root, text = date_time.strftime('%m %B'))
 
 # Daytimes (hours)
 
-time_hour = Label(root, text=date_time.strftime('%I : %M %p'), bg='white', font=("bold", 15))
+time_hour = Label(root, text=date_time.strftime('%I : %M %p'))
 
 # Location research
 
@@ -29,14 +29,29 @@ input_city = Entry(root, textvariable = name_city, width=45)
 def name_city():
     
     # Request API
-    
     api_key = "12f04c87d16f8e311477842c595d4c77"
     base_url = "https://api.openweathermap.org/data/2.5/weather?q="
     request_weather_data = requests.get(base_url + input_city.get() + "&units=metric&appid=" + api_key)
-    x = json.loads(request_weather_data.content)
+    api_result = json.loads(request_weather_data.content)
+
+    # Coordinates long, lat
+    xcoord = api_result['coord']
+    longtitude = xcoord['lon']
+    latitude = xcoord['lat']
 
     # Temp
-    
+    ytemp = api_result['main']
+    temp_now = ytemp['temp']
+    humidity = ytemp['humidity']
+    minimalTemp = ytemp['temp_min']
+    maximalTemp = ytemp['temp_max']
+
+    # Country (Bsp. Deutschland) + city(Bsp. Lörrach)
+    z_api = api_result['sys']
+    api_country = z_api['country']
+    api_city = api_result['name']
+
+
 root.mainloop()
 
 """ /------ Auskommentiert ---------/
