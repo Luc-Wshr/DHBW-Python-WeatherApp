0bf5e51013286e6c340459aedc62df943029b34d
Luc Weishar <71008362+Luc-Wshr@users.noreply.github.com>
2021-06-02 09:38:27 +0200

    Delete Benachrichtigung.py

--- Code/Benachrichtigung.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-from tkinter import messagebox
-# hide main window
-root = tkinter.Tk()
-# message box display
-messagebox.showerror("Error", "Error message")
-messagebox.showwarning("Warning","Warning message")
-messagebox.showinfo("Information","Informative message")
