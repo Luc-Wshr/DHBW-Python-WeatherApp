a54c58b9dee89267394c13b55ec09e05d18e17ff
Luc-Wshr <luc.weishar@cte.ch>
2021-06-09 10:12:31 +0200

    added day/night difference

--- Code/Main.py
+++ Code/Main.py
@@ -221,7 +221,12 @@ def save_as_favorite():
 
 
 #----------------------------------------------------------------------------------------Image
-img = ImageTk.PhotoImage(Image.open("Code/settings/Logo_Python.png"))
+mytime = time.localtime()
+if mytime.tm_hour < 6 or mytime.tm_hour > 18:
+    img = ImageTk.PhotoImage(Image.open("Code/settings/Logo_Python_night.png"))
+else:
+    img = ImageTk.PhotoImage(Image.open("Code/settings/Logo_Python.png"))
+
 logo = Label(root, pady=100, image=img)
 logo.grid(row=0, column=0, sticky=W)
 
